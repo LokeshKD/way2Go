506da542e2764e4e6fd0b5248c61c4f471cdaffc
Lokesh <i.lokesh@gmail.com>
2021-08-26 16:44:06 +0530

    switching...

---
+++ basics/switch_.go
@@ -0,0 +1,40 @@
+package main
+import "fmt"
+
+func main() {
+    var num1 int = 100
+    // Adding switch on num1
+    switch num1 {
+        case 98, 99:        // first case: num1 = 98 or 99
+            fmt.Println("It's equal to 98")
+        case 100:           // second case: num1 = 100
+            fmt.Println("It's equal to 100")
+        default:            // optional/ default case
+            fmt.Println("It's not equal to 98 or 100")
+    }
+
+    //switch construct with conditions...
+    switch {
+        case num1 < 0:
+            fmt.Println("Number is negative")
+
+        case num1 > 0 && num1 < 10:
+            fmt.Println("Number is between 0 and 10")
+
+        default:
+            fmt.Println("Number is 10 or greater")
+    }
+
+    // initialization within switch block
+    switch numr21 := 100;{
+        case numr21 < 0:
+            fmt.Println("Number is negative")
+
+        case numr21 > 0 && num1 < 10:
+            fmt.Println("Number is between 0 and 10")
+
+        default:
+            fmt.Println("Number is 10 or greater")
+    }
+}
+
