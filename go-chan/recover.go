4e3e820d81d2c64efb9c9c0b1fda31cd48a89433
Lokesh <i.lokesh@gmail.com>
2021-11-07 16:39:46 +0530

    reovering from a panicking goroutine without affecting others

---
+++ go-chan/recover.go
@@ -0,0 +1,17 @@
+// Example usage of using recover with goroutine(s)
+
+func server(workChan <-chan *Work) {
+  for work := range workChan {
+    go safelyDo(work) // start the goroutine for that work
+  }
+}
+
+func safelyDo(work *Work) {
+  defer func() {
+   if err := recover(); err != nil {
+     log.Printf("work failed with %s in %v:", err, work)
+     // the failing goroutine is stopped
+   }
+ }()
+do(work)
+}
